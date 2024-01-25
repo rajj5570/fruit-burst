5b2abdc58500682d8fc9559d65e358d2ad2be5d9
rajib5570 <rajib5570@gmail.com>
2024-01-25 21:02:13 +0600

    responsive navbar added for 5 different types of screen-width

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        fontFamily:{
+            'lato': ['lato', 'system-ui'],
+        }
+        color:{
+            'main-color' : ['#F85559']
+        }
+      extend: {
+
+      },
+    },
+    plugins: [],
+  }
